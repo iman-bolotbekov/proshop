ac96ed25011b8e5eeb8326cebcef89dc31caca4b
iman-bolotbekov <iman114@gmail.com>
2023-07-24 13:27:55 +0600

    configured for deploy to vercel

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-1vlsl*@x5q_j=rof+yv3*(iyz4tfbaz$bf=(!qr7%#q-h246)v
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 # Application definition
 
